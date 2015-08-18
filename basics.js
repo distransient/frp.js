fac6ef7d51722162c6ca77304836e73bc1d9170a
kel <humanoidanalog@gmail.com>
2015-08-17 22:37:31 -0700

    add link to elm example, make basic example more recognizable

--- basics.js
+++ basics.js
@@ -1,5 +1,5 @@
 var $ = document.querySelector.bind(document)
   , model = { value: 0 }
 
-$('button').onclick = () => { model.value += 1 }
-Object.observe(model, () => { $('output').textContent = model.value })
+$('button').onclick = function() { model.value += 1 }
+Object.observe(model, function() { $('output').textContent = model.value })
