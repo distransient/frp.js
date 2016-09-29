0e68fc0f226cedba8408d435716184a3a8e433ba
Kel <kel@unclear.info>
2016-09-29 14:09:07 -0700

    Fix null ship reference

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var ship = document.querySelector('img[ship]')
+var ship = document.querySelector('[ship]')
   , velocity = { x: (Math.random - 0.5) * 2, y: 0, rotation: 0 }
   , model = { x: 10, y: 10, rotation: 0 }
 
@@ -18,7 +18,7 @@ window.setInterval(function () {
   model.x += velocity.x
   model.y += velocity.y
   model.rotation += velocity.rotation
-ship.style.bottom = model.y + "em"
-ship.style.left = model.x  + "em"
-ship.style.transform = 'rotate(' + model.rotation % 360 + 'deg)'
+  ship.style.bottom = model.y + "em"
+  ship.style.left = model.x  + "em"
+  ship.style.transform = 'rotate(' + model.rotation % 360 + 'deg)'
 }, 0)
