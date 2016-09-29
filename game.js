676f4f5920c5e03de1d44ff9ddde9c559a194af8
Kel <kel@unclear.info>
2016-09-29 14:07:23 -0700

    Fix game.js formatting

--- game.js
+++ game.js
@@ -14,13 +14,11 @@ Object.observe(model, function () {
   ship.style.transform = 'rotate(' + model.rotation % 360 + 'deg)'
 })
 
-  window.setInterval(function () {
-    model.x += velocity.x
-    model.y += velocity.y
-    model.rotation += velocity.rotation
-  ship.style.bottom = model.y + "em"
-  ship.style.left = model.x  + "em"
-  ship.style.transform = 'rotate(' + model.rotation % 360 + 'deg)'
-  }, 0)
-
-  */
+window.setInterval(function () {
+  model.x += velocity.x
+  model.y += velocity.y
+  model.rotation += velocity.rotation
+ship.style.bottom = model.y + "em"
+ship.style.left = model.x  + "em"
+ship.style.transform = 'rotate(' + model.rotation % 360 + 'deg)'
+}, 0)
